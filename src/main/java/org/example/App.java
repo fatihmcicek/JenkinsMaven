9c303ded15f3381590d6fd3e77f7f763b04e152d
fatihmcicek <fatihmcicek5@gmail.com>
2023-08-05 01:07:50 +0300

    for jenkins build

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,22 @@
 package org.example;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Notest");
-        System.out.println(new App().Add(2,3));
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Fatih Mehmet CICEK");
+        System.out.println(new App().Add(2, 3));
+        System.out.println(new App().Multiply(2, 3));
+        System.out.println(new App().Subtract(2, 3));
     }
 
-    public int Add(int number1, int number2){
+    public int Add(int number1, int number2) {
         return number1 + number2;
     }
 
-    public int Subtract(int number1, int number2){
+    public int Subtract(int number1, int number2) {
         return number1 - number2;
     }
 
-    public int Multiply(int number1, int number2){
+    public int Multiply(int number1, int number2) {
         return number1 * number2;
     }
 }
